c96817ffa0ff1a43a128784197faccbf35f34e6c
FranciscoGlaizel <polarismo25@gmail.com>
2022-03-10 12:43:38 +0800

    My Educational Background Added

--- git-python-francisco.py
+++ git-python-francisco.py
@@ -8,3 +8,7 @@ Created on Thu Mar 10 11:41:03 2022
 print ("Glaizel Francisco")
 print ("Dawis, Gasan, Marinduque") 
 print ("April 19, 2002")
+
+print ("Marinduque State College")
+print ("Bachelor of Science in Information Technology")
+print ("2021-2022")
