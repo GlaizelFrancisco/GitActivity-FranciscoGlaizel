91d1575e76be8503ecc2473e58b676c84222743b
FranciscoGlaizel <polarismo25@gmail.com>
2022-03-10 12:26:06 +0800

    My Personal Info added

---
+++ git-python-francisco.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 11:41:03 2022
+
+@author: Glaizel
+"""
+
+print ("Glaizel Francisco")
+print ("Dawis, Gasan, Marinduque") 
+print ("April 19, 2002")
